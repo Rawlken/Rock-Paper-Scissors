7ed3a5368f2c88ba81a3138fd02c32b933883dd3
Rawlken <117388813+Rawlken@users.noreply.github.com>
2023-03-27 20:13:40 +0300

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,39 @@
+import random
+choice = "Rock","Paper","Scissors"
+print('''
+Type 1 to Select Rock.
+Type 2 to Select Paper.
+Type 3 to Select Scissors.
+If you want to exit the game press the "q" key and press enter.
+''')
+
+while True:
+    com_choice = random.choice(choice)
+    player_choice = input("Type Your Choice: ")
+    
+    if(player_choice == "1"):
+        if(com_choice == "Rock"):
+            print("Draw")
+        elif(com_choice == "Paper"):
+            print("Swallowed You")
+        elif(com_choice == "Scissors"):
+            print("You Smashed")
+    
+    elif(player_choice == "2"):
+        if(com_choice == "Rock"):
+            print("You Swallowed It")
+        elif(com_choice == "Paper"):
+            print("You Are Of The Same Mind")
+        elif(com_choice == "Scissors"):
+            print("It Tore You Apart")
+    
+    else:
+        if(com_choice == "Rock"):
+            print("Turned You To Dust")
+        elif(com_choice == "Paper"):
+            print("You Smashed It")
+        elif(com_choice == "Scissors"):
+            print("Draw")
+    
+    if(player_choice == "q"):
+        quit()
